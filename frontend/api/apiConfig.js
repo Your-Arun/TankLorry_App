6669b0da8be5f1764f0fe465ff5746b9c153d298
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2026-02-27 13:48:11 +0530

    fgfg

--- frontend/api/apiConfig.js
+++ frontend/api/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://192.168.1.12:5000/api';
+const BASE_URL = 'https://tanklorry-app.onrender.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
